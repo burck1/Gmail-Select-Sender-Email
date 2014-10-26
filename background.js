3943201206d38f528c1f747bac693d9aaad01ace
Alex Burck <burck1@illinois.edu>
2014-10-26 13:41:30 -0500

    now supports previous email in thread

--- background.js
+++ background.js
@@ -5,28 +5,27 @@ function itemClick(item, email) {
     input.focus();
     input.select();
     input.keyup(function(e) {
-        if (e.which === 17 || e.which === 91 || e.which === 93) {
+        if (e.which === 27 ||  e.which === 17 || e.which === 91 || e.which === 93) {
             $(this).replaceWith(orig);
             setupEmailSelect();
         }
     });
 }
 function setupEmailSelect() {
-    var sel = $(".acZ").find(".gD");
+    var sel = $("td.gF.gK .gD");
     if (sel) {
-        var arr = sel.get();
-        if (arr.length > 0) {
-            var email = arr[0].getAttribute('email');
-            sel.click(function(e) {
+        sel.each(function() {
+            var email = $(this).get(0).getAttribute('email');
+            $(this).click(function(e) {
                 itemClick($(this), email);
             });
-            var sel2 = $(".acZ").find(".go");
+            var sel2 = $(this).next(".go");
             if (sel2) {
                 sel2.click(function(e) {
                     itemClick($(this), email);
                 });
             }
-        }
+        });
     }
 }
 
